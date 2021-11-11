8ac7621d8b7a411b5239fe03e056aaf313395674
Alex Martinez <alexkmartinez77@gmail.com>
2021-11-10 21:25:05 -0800

    Organizes movie data into cards.

--- src/js/components/Movies/Movie.jsx
+++ src/js/components/Movies/Movie.jsx
@@ -5,8 +5,17 @@ export default class Movies extends React.Component {
   render() {
     const { Title, Poster, Year} = this.props.movieData;
     return (
-      <div>
-        { Title } { Poster } { Year }
+      <div className='card my-3'>
+        <div className='row'>
+          <div className='col-sm-6 col-md-6 col-lg-4 text-center p-3'>
+            <img src={ Poster }></img>
+          </div>
+          <div className='col-sm-6 col-md-6 col-lg-8'>
+            <h1 className='display-6 text-center'>{ Title }</h1>
+            <hr></hr>
+            <div>{ Year }</div>
+          </div>
+        </div>
       </div>
     );
  }
