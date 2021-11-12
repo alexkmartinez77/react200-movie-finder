872fcfccfe3a94c1bbdb243ee42577efa1497c22
Alex Martinez <alexkmartinez77@gmail.com>
2021-11-12 14:18:53 -0800

    Adds plot to Movie Search containers and adds button (More Information) to redirect to individual movie details.

--- src/js/components/Movies/Movie.jsx
+++ src/js/components/Movies/Movie.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Movies extends React.Component {
 
   render() {
-    const { Title, Poster, Year} = this.props.movieData;
+    const { Title, Poster, Year, Plot} = this.props.movieData;
     return (
       <div className='card my-3'>
         <div className='row'>
@@ -12,8 +12,10 @@ export default class Movies extends React.Component {
           </div>
           <div className='col-sm-6 col-md-6 col-lg-8'>
             <h1 className='display-6 text-center'>{ Title }</h1>
+            <h1 className='display-6 text-center'>{ Year }</h1>
             <hr></hr>
-            <div>{ Year }</div>
+            <div>{ Plot }</div>
+            <div className='float-right m-3'><button className='btn btn-info'>More Information</button></div>
           </div>
         </div>
       </div>
